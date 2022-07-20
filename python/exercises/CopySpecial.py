f43ad686ac27fb198c80dc90c69caaadc35ce75b
Hrishikesh Mohan <hrishikesh.mohan@aptonworks.com>
2022-07-20 12:00:58 +0530

    complete

---
+++ python/exercises/CopySpecial.py
@@ -0,0 +1,127 @@
+"""CopySpecial
+Exercise
+The copyspecial.py program takes one or more directories as its arguments. We'll say that a "special" file is one 
+where the name contains the pattern __w__ somewhere, where the w is one or more word chars. 
+The provided main() includes code to parse the command line arguments, but the rest is up to you. 
+Write functions to implement the features below and modify main() to call your functions.
+
+Suggested functions for your solution(details below):
+
+    get_special_paths(dir) -- returns a list of the absolute paths of the special files in the given directory
+    copy_to(paths, dir) given a list of paths, copies those files into the given directory
+    zip_to(paths, zippath) given a list of paths, zip those files up into the given zipfile
+
+Part A (manipulating file paths)
+
+Gather a list of the absolute paths of the special files in all the directories. 
+In the simplest case, just print that list (here the "." after the command is a single argument indicating the current directory). 
+Print one absolute path per line.
+
+
+
+We'll assume that names are not repeated across the directories (optional: check that assumption and error out if it's violated).
+
+Part B (file copying)
+If the "--todir dir" option is present at the start of the command line, do not print anything and instead copy the files to the given directory, 
+creating it if necessary. Use the python module "shutil" for file copying.
+
+
+Part C (calling an external program)
+
+If the "--tozip zipfile" option is present at the start of the command line, run this command: "zip -j zipfile <list all the files>". 
+This will create a zip file containing the files. Just for fun/reassurance, also print the command line you are going to do first (as shown in the lecture). 
+(Windows note: windows do not come with a program to produce standard .zip archives by default, but you can get download the free and 
+open zip program from www.info-zip.org.)
+
+If the child process exits with an error code, exit with an error code and print the command's output. 
+Test this by trying to write a zip file to a directory that does not exist.
+"""
+
+ 
+import sys
+import re
+import os
+import shutil
+import commands
+
+#Copy Special exercise
+
+# +++your code here+++
+# Write functions and modify main() to call them
+
+def list_dir_files(dir):
+  res = []
+  files = os.listdir(dir)
+  for x in files:
+    match = re.search(r'__(\w+)__', x)
+    if(match):
+      res.append(os.path.abspath(os.path.join(dir, x)))
+  return res
+
+def copy(c_dirs, todir):
+  if(os.path.isdir(todir)):
+    for path in c_dirs:
+      fname = os.path.basename(path)
+      shutil.copy(path, os.path.join(todir, fname))
+  else:
+    os.mkdir(todir)
+    for path in c_dirs:
+      fname = os.path.basename(path)
+      shutil.copy(path, os.path.join(todir, fname))
+  return 0
+
+def zip(c_dirs, tozip):
+  cmd = 'zip -j '+tozip+' '.join(c_dirs)
+  stat,out = commands.getstatusoutput(cmd)
+  if(stat):
+    sys.stderr.write(out)
+    sys.exit(1)
+  return 0
+
+def main():
+  # This basic command-line argument parsing code is provided.
+  # Add code to call your functions below.
+
+  # Make a list of command line arguments, omitting the [0] element
+  # which is the script itself.
+  args = sys.argv[1:]
+  if not args:
+    print("usage: [--todir dir][--tozip zipfile] dir [dir ...]")
+    sys.exit(1)
+
+
+  # todir and tozip are either set from command line
+  # or left as the empty string.
+  # The args array is left just containing the dirs.
+  todir = ''
+  tozip = ''
+
+  if args[0] == '--todir':
+    todir = args[1]
+    del args[0:2]
+  elif args[0] == '--tozip':
+    tozip = args[1]
+    del args[0:2]
+  else:
+    print("error: must specify one or more dirs")
+    sys.exit(1)
+
+
+  c_dirs = []
+  #dirs = list_dir_files(c_dirs)
+  for x in args:
+    c_dirs.extend(list_dir_files(x))
+    print(x,"\n")
+  
+  if(todir != ''):
+    copy(c_dirs, todir)
+  elif(tozip != ''):
+    zip(c_dirs, tozip)
+  else:
+    for x in c_dirs:
+      print(x+"\n")
+  # +++your code here+++
+  # Call your functions
+ 
+if __name__ == "__main__":
+  main()
---
+++ python/exercises/logPuzzle.py
@@ -0,0 +1,144 @@
+# Log Puzzle
+# Exercise
+# For the Log Puzzle exercise, you'll use Python code to solve two puzzles. This exercise uses the urllib module, as shown in the Python utility section. 
+# The files for this exercise are in the "log puzzle" directory inside google-python-exercises (download the google-python-exercises.zip 
+# if you have not already, see Set Up for details). Add your code to the "logpuzzle.py" file.
+
+# An image of an animal has been broken into many narrow vertical stripe images. The stripe images are on the internet somewhere, each with its own URL. 
+# The URLs are hidden in a web server log file. Your mission is to find the URLs and download all image stripes to re-create the original image.
+
+# The slice URLs are hidden inside apache log files (the open-source Apache web server is the most widely used server on the internet). 
+# Each log file is from some server, and the desired slice URLs are hidden within the logs. The log file encodes what server it comes from like this: 
+# the log file animal_code.google.com is from the code.google.com server (formally, we'll say that the server name is whatever follows the first underbar). 
+# The animal_code.google.com log file contains the data for the "animal" puzzle image. Although the data in the log files has the syntax of a real apache web server, 
+# the data beyond what's needed for the puzzle is randomized data from a real log file.
+
+# Here is what a single line from the log file looks like (this really is what apache log files look like):
+
+
+
+# The first few numbers are the address of the requesting browser. The most interesting part is the "GET path HTTP" showing the path of a web request 
+# received by the server. The path itself never contains spaces and is separated from the GET and HTTP by spaces (regex suggestion: \S (upper case S) 
+# matches any non-space char). Find the lines in the log where the string "puzzle" appears inside the path, ignoring the many other lines in the log.
+
+# Part A - Log File To Urls
+# Complete the read_urls(filename) function that extracts the puzzle URLs from inside a logfile. Find all the "puzzle" path URLs in the log file. 
+# Combine the path from each URL with the server name from the filename to form a full URL, e.g. "http://www.example.com/path/puzzle/from/inside/file". 
+# Screen out URLs that appear more than once. The read_urls() function should return the list of full URLs, sorted in alphabetical order and without duplicates. 
+# Taking the URLs in alphabetical order will yield the image slices in the correct left-to-right order to re-create the original animal image. 
+# In the simplest case, main() should just print the URLs, one per line.
+
+# $ ./logpuzzle.py animal_code.google.com
+
+
+# ...
+
+
+# Part B - Download Images Puzzle
+# Complete the download_images() function which takes a sorted list of URLs and a directory. Download the image from each URL into the given directory, 
+# creating the directory first if necessary (see the "os" module to create a directory, and "urllib.urlretrieve()" for downloading a URL). 
+# Name the local image files with a simple scheme like "img0", "img1", "img2", and so on. You may wish to print a little "Retrieving..." 
+# status output line while downloading each image since it can be slow and it's nice to have some indication that the program is working. 
+# Each image is a little vertical slice from the original. How to put the slices together to re-create the original? 
+# It can be solved nicely with a little HTML (knowledge of HTML is not required).
+
+# The download_images() function should also create an index.html file in the directory with an *img* tag to show each local image file. 
+# The IMG tags should all be on one line together without separation. In this way, the browser displays all the slices together seamlessly. 
+# You do not need knowledge of HTML to do this; just create an index.html file that looks like this:
+
+
+
+# Here's what it should look like when you can download the animal puzzle:
+
+
+
+# When it's all working, opening the index.html in a browser should reveal the original animal image. What is the animal in the image?
+
+# Part C - Image Slice Descrambling
+# The second puzzle involves an image of a very famous place but depends on some custom sorting. For the first puzzle, 
+# the URLs can be sorted alphabetically to order the images correctly. In the sort, the whole URL is used. However, 
+# we'll say that if the URL ends in the pattern "-wordchars-wordchars.jpg", e.g. "", then the URL should be represented by the second word in the sort (e.g. "baaa").
+# So sorting a list of URLs each ending with the word-word.jpg pattern should order the URLs by the second word.
+
+import os
+import re
+import sys
+import urllib
+
+"""Logpuzzle exercise
+Given an apache logfile, find the puzzle urls and download the images.
+
+Here's what a puzzle url looks like:
+10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
+"""
+
+
+def read_urls(filename):
+  """Returns a list of the puzzle urls from the given log file,
+  extracting the hostname from the filename itself.
+  Screens out duplicate urls and returns the urls sorted into
+  increasing order."""
+  fp = open(filename,'r')
+  text = fp.read()
+  fp.close()
+  hostname = "http://"+filename.split('_')[1]
+  links = []
+  imgs = re.findall('([-\w]+\.(?:jpg|gif|png))', text)
+  for x in imgs:
+    links.append(hostname+imgs)
+  return sorted(links)    
+  # +++your code here+++
+ 
+
+def download_images(img_urls, dest_dir):
+  """Given the urls already in the correct order, downloads
+  each image into the given directory.
+  Gives the images local filenames img0, img1, and so on.
+  Creates an index.html in the directory
+  with an img tag to show each local image file.
+  Creates the directory if necessary.
+  """
+  if not os.path.exists(dest_dir):
+    os.mkdir(dest_dir)
+  os.chdir(dest_dir)
+
+  image_tags = []
+
+  for url in img_urls:
+      image_name = url.split("/")[-1]
+
+      response = urllib.request.urlopen(url)
+
+      image = open(image_name, "wb")
+      image.write(response.read())
+
+      image_tags.append('<img src="{0}">'.format(image_name))
+
+  html_file = open("index.html", "w")
+  html_file.write("<html><body>{0}</body></html>".format(''.join(image_tags)))
+
+  pass
+  # +++your code here+++
+ 
+
+def main():
+  args = sys.argv[1:]
+
+  if not args:
+    print('usage: [--todir dir] logfile ')
+    sys.exit(1)
+
+  todir = ''
+  if args[0] == '--todir':
+    todir = args[1]
+    del args[0:2]
+
+  img_urls = read_urls(args[0])
+
+  if todir:
+    download_images(img_urls, todir)
+  else:
+    print('\n'.join(img_urls))
+
+if __name__ == '__main__':
+  main()
